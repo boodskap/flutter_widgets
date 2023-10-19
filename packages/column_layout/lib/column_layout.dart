8e1d56bf2dfdc1c48f1d2ccbb1c95eebca8d6f95
jeganvicent <jvincent@boodskap.io>
2023-10-19 10:30:57 +0530

    initial commit

--- packages/column_layout/lib/column_layout.dart
+++ packages/column_layout/lib/column_layout.dart
@@ -2,26 +2,51 @@ library column_layout;
 
 import 'package:flutter/material.dart';
 
+/// callback method to render a child widget on-demand
 typedef GetColumnWidgetAt = Widget Function(int column, int row);
+
+// callback method to set the alignment of a layout cell dynamically
 typedef GetColumnAlignmentAt = Alignment Function(int column, int row);
 
+/// A simple layout where you can specify the number of columns and then rows in each columns
+///
+/// Very complex layouts can be achieved by combinations of RowLayout and [ColumnLayout](https://pub.dev/packages/column_layout)
+//ignore: must_be_immutable
 class ColumnLayout extends StatefulWidget {
-  ColumnLayout(
-      {super.key,
-      required this.columns,
-      required this.rows,
-      required this.columnFlex,
-      required this.rowFlex,
-      required this.getColumnWidgetAt,
-      required this.getColumnAlignmentAt,
-      this.padding = const EdgeInsets.all(0.0),
-      this.decoration = const BoxDecoration(
-          border: Border(
-              top: BorderSide(color: Colors.black),
-              bottom: BorderSide(color: Colors.black),
-              right: BorderSide(color: Colors.black),
-              left: BorderSide(color: Colors.black))),
-      this.background = Colors.white});
+  ///
+  /// **columns** the number of columns this layout should have
+  ///
+  /// **rows** the number of rows each column should have, the size of the array should match the **columns**
+  ///
+  /// **columnFlex** the width percentage of each column should have,  the size of the array should match the **columns**
+  ///
+  /// **rowFlex** the height percentage of each row should have,  the size of the map should match the **columns** and the size of the array should match the **rows** in that column
+  ///
+  /// **getColumnWidgetAt** A callback method to dynamically render the widget at (column, row) cell
+  ///
+  /// **getColumnAlignmentAt** aligment of a cell
+  ///
+  /// **padding** Example: const EdgeInsets.all(10.0), default is const EdgeInsets.all(0.0)
+  ///
+  /// **background** Example: Colors.read, default is Colors.white
+  ///
+  ColumnLayout({
+    super.key,
+    required this.columns,
+    required this.rows,
+    required this.columnFlex,
+    required this.rowFlex,
+    required this.getColumnWidgetAt,
+    required this.getColumnAlignmentAt,
+    this.background = Colors.white,
+    this.padding = const EdgeInsets.all(0.0),
+    this.decoration = const BoxDecoration(
+        border: Border(
+            top: BorderSide(color: Colors.black),
+            bottom: BorderSide(color: Colors.black),
+            right: BorderSide(color: Colors.black),
+            left: BorderSide(color: Colors.black))),
+  });
 
   final int columns;
   final List<int> rows;
@@ -29,14 +54,15 @@ class ColumnLayout extends StatefulWidget {
   final Map<int, List<int>> rowFlex;
   final GetColumnWidgetAt getColumnWidgetAt;
   final GetColumnAlignmentAt getColumnAlignmentAt;
-  final Color? background;
-  final EdgeInsetsGeometry? padding;
-  final Decoration? decoration;
+  Color? background;
+  EdgeInsetsGeometry? padding;
+  Decoration? decoration;
 
   @override
   State<ColumnLayout> createState() => _ColumnLayoutState();
 }
 
+/// internal widget state
 class _ColumnLayoutState extends State<ColumnLayout> {
   Widget _createPlacement(int row, int col, int flex, Widget child) {
     return Expanded(
@@ -53,6 +79,7 @@ class _ColumnLayoutState extends State<ColumnLayout> {
     );
   }
 
+  /// method to build a column along with its dynamic widget
   Widget _buildColumn(int idx, int flex) {
     List<Widget> children = [];
 
@@ -67,6 +94,7 @@ class _ColumnLayoutState extends State<ColumnLayout> {
     );
   }
 
+  /// method to muild all the columns of this layout
   Widget _buildColumns() {
     List<Widget> children = [];
 
@@ -84,6 +112,7 @@ class _ColumnLayoutState extends State<ColumnLayout> {
     );
   }
 
+  /// default method to build the widget
   @override
   Widget build(BuildContext context) {
     return Stack(
